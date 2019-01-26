6df834ca51a1c48af00b6f8957f893823de30fe3
shourav45 <shourav45@gmail.com>
2019-01-27 03:03:06 +0600

    rate crud

--- WebApi/Models/RateInfo.cs
+++ WebApi/Models/RateInfo.cs
@@ -13,9 +13,9 @@ namespace WebApi.Models
         public int RateInfoId { get; set; }
         public string PartyInfoId { get; set; }
         public string RateType { get; set; }
-        public string FirstKP { get; set; }
-        public string FirstKPRate { get; set; }
-        public string AfterFirstKPRate { get; set; }
+        public string FirstKP { get; set; }//for poly large
+        public string FirstKPRate { get; set; }//for poly medium
+        public string AfterFirstKPRate { get; set; }//for poly small
         public string AddBy { get; set; }
         public string AddDate { get; set; }
         public string Ex1 { get; set; }
