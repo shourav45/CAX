4571c35f617fa1c78a70355a0242f49d09e1edcc
shourav45 <shourav45@gmail.com>
2019-02-03 22:57:48 +0600

    rpt

--- WebApi/Controllers/ReportController.cs
+++ WebApi/Controllers/ReportController.cs
@@ -1,58 +1,58 @@
-﻿//using Microsoft.Reporting.WebForms;
-//using System;
-//using System.Collections.Generic;
-//using System.IO;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
-//using WebApi.Models;
-//using WebApi.Models.ViewModels;
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApi.Models;
+using WebApi.Models.ViewModels;
 
-//namespace WebApi.Controllers
-//{
-//    public class ReportController : Controller
-//    {
-//        private dbcontext db;
-//        // GET: Report
-//        public ActionResult Index()
-//        {
-//            return View();
-//        }
+namespace WebApi.Controllers
+{
+    public class ReportController : Controller
+    {
+        private dbcontext db;
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
 
-//        public ActionResult DayReport()
-//        {
-//            return View();
-//        }
+        public ActionResult DayReport()
+        {
+            return View();
+        }
 
-//        public ActionResult GetDailyReport()
-//        {
-//            db = new dbcontext();
-//            List<CNInfo> cnData = db.CNInfoset.ToList();
-//            //var rptData = from c in db.CNInfoset
-//            //              join p in db.PartyInfoset on c.PartyId equals p.PartyInfoId
-//            //              select new VMDailyInfo
-//            //              {
-//            //                  Amount = Convert.ToDecimal(c.TotalAmount),
-//            //                  PartyName = p.Name
-//            //              };
-            
+        public ActionResult GetDailyReport()
+        {
+            db = new dbcontext();
+            List<CNInfo> cnData = db.CNInfoset.ToList();
+            //var rptData = from c in db.CNInfoset
+            //              join p in db.PartyInfoset on c.PartyId equals p.PartyInfoId
+            //              select new VMDailyInfo
+            //              {
+            //                  Amount = Convert.ToDecimal(c.TotalAmount),
+            //                  PartyName = p.Name
+            //              };
 
-//            //for rdlc
-//            Warning[] warnings;
-//            string mimeType;
-//            string[] streamids;
-//            string encoding;
-//            string filenameExtension;
 
-//            var viewer = new ReportViewer();
-//            viewer.LocalReport.ReportPath = Server.MapPath("~/RDLC/rptDailySales.rdlc");
+            //for rdlc
+            Warning[] warnings;
+            string mimeType;
+            string[] streamids;
+            string encoding;
+            string filenameExtension;
 
-//            viewer.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", cnData));
-//            viewer.LocalReport.Refresh();
+            var viewer = new ReportViewer();
+            viewer.LocalReport.ReportPath = Server.MapPath("~/RDLC/rptDailySales.rdlc");
 
-//            var bytes = viewer.LocalReport.Render("PDF", null, out mimeType, out encoding, out filenameExtension, out streamids, out warnings);
+            viewer.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", cnData));
+            viewer.LocalReport.Refresh();
 
-//            return File(bytes, mimeType);
-//        }
-//    }
-//}
+            var bytes = viewer.LocalReport.Render("PDF", null, out mimeType, out encoding, out filenameExtension, out streamids, out warnings);
+
+            return File(bytes, mimeType);
+        }
+    }
+}
